a45b4a7214ec51af12828538407d9cbcaf5d4d47
Jim Disenstam <jim.disenstam@gmail.com>
2019-04-03 10:08:10 +0200

    Adding min and mean

--- DescriptiveStatistics/Statistics.cs
+++ DescriptiveStatistics/Statistics.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DescriptiveStatistics
@@ -8,17 +9,50 @@ namespace DescriptiveStatistics
     /// </summary>
     public static class Statistics
     {
+        /// <summary>
+        /// Returns an object containing descriptive statistics based on a source of integers.
+        /// </summary>
+        /// <param name="source">The integers to analyze.</param>
+        /// <returns></returns>
         public static dynamic DescriptiveStatistics(int[] source)
         {
             int maximum = Maximum(source);
+            int minimum = Minimum(source);
+            double mean = Mean(source);
 
             var summary = new
             {
-                Maximum = maximum
+                Maximum = maximum,
+                Minimum = minimum,
+                Mean = mean
             };
 
             return summary;
         }
+
+        /// <summary>
+        /// Returns the maximum value from an array of integers.
+        /// </summary>
+        /// <param name="source">The integers to analyze.</param>
+        /// <returns></returns>
         public static int Maximum(int[] source) => source.Max();
+
+        /// <summary>
+        /// Returns the minimum value from an array of integers.
+        /// </summary>
+        /// <param name="source">The integers to analyze.</param>
+        /// <returns></returns>
+        public static int Minimum(int[] source) => source.Min();
+
+        /// <summary>
+        /// Returns the mean from an array of integers.
+        /// </summary>
+        /// <param name="source">The integers to analyze.</param>
+        /// <returns></returns>
+        public static double Mean(int[] source)
+        {
+            var list = new List<int>(source);
+            return list.Average();
+        }
     }
 }
