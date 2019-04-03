557622583e2040e7ba23561d8dcaa069b7a8ff85
Jim Disenstam <jim.disenstam@gmail.com>
2019-04-03 09:26:19 +0200

    Getting int[] from json file working

---
+++ DescriptiveStatistics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DescriptiveStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string json = File.ReadAllText(args[0]);
+                int[] statistics = JsonConvert.DeserializeObject<int[]>(json);
+                Console.WriteLine(statistics[0] + statistics[1]); // 106
+            }
+        }
+    }
+}
